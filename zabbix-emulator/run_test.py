ce767b28286cd87e74b8e01e1627893bcfc6d8a7
Kazuhiro Yamato <kz0817@gmail.com>
2015-11-04 18:00:12 +0900

    implemented (to be squashed).

--- zabbix-emulator/run_test.py
+++ zabbix-emulator/run_test.py
@@ -4,6 +4,8 @@ import subprocess
 import logging
 import sys
 import traceback
+import time
+import signal
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -16,6 +18,8 @@ class Manager(object):
         self.__proc_zabbix_emu = None
         self.__proc_simple_sv = None
 
+        signal.signal(signal.SIGCHLD, self.__child_handler)
+
     def __del__(self):
         def terminate(proc):
             if proc is None:
@@ -26,18 +30,28 @@ class Manager(object):
         for proc in (self.__proc_zabbix_emu, self.__proc_simple_sv):
             terminate(proc)
 
-    def __call__(self):
+    def __child_handler(self, signum, frame):
+        logger.error("Got SIGCHLD: %s")
+        assert False
 
+    def __call__(self):
         # boot zabbix_emulator
         zabbix_emulator_args = "%s" % self.__args.zabbix_emulator_path
-        self.__proc_zabbix_emu = subprocess.Popen(zabbix_emulator_args)
+        kw = {
+            "stdout": self.__args.zabbix_emulator_log,
+            "stderr": subprocess.STDOUT,
+        }
+        self.__proc_zabbix_emu = subprocess.Popen(zabbix_emulator_args, **kw)
         logger.info("Launched zabbix emulator: PID: %s" % \
-                    self.__proc_zabbix_emu)
+                    self.__proc_zabbix_emu.pid)
 
         # boot simple_server
         simple_server_args = "%s" % self.__args.simple_server_path
         self.__proc_simple_sv = subprocess.Popen(simple_server_args)
-        logger.info("Launched simple server: PID: %s" % self.__proc_simple_sv)
+        logger.info("Launched simple server: PID: %s" % \
+                    self.__proc_simple_sv.pid)
+
+        time.sleep(1000) # temporary
 
 
 def main():
@@ -46,6 +60,9 @@ def main():
                         default="zabbix_emulator.py")
     parser.add_argument("-s", "--simple-server-path", type=str,
                         default="simple_server.py")
+    parser.add_argument("-l", "--zabbix-emulator-log",
+                        type=argparse.FileType('w'),
+                        default="zabbix-emulator.log")
     args = parser.parse_args()
 
     manager = Manager(args)
